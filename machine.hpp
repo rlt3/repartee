e6773be799d2e7ea203e8348b99a2cc1786773ed
rlt3 <rlt3@utk.edu>
2019-05-15 18:01:43 -0400

    adding base for load/store instructions to add variables

--- machine.hpp
+++ machine.hpp
@@ -8,12 +8,14 @@ typedef uint8_t Opcode;
 
 enum InsOpcode {
     OP_HALT = 0x0,
-    OP_PUSH = 0x1,
-    OP_POP  = 0x2,
-    OP_ADD  = 0x3,
-    OP_SUB  = 0x4,
-    OP_DIV  = 0x5,
-    OP_MUL  = 0x6
+    OP_PUSH = 0x1, /* push immediate onto stack */
+    OP_POP  = 0x2, /* pop top of stack */
+    OP_ADD  = 0x3, /* pop n values off stack, add them, and push result */
+    OP_SUB  = 0x4, /* pop n values off stack, subtract them, and push result */
+    OP_DIV  = 0x5, /* pop n values off stack, divide them, and push result */
+    OP_MUL  = 0x6, /* pop n values off stack, multiply them, and push result */
+    OP_LOAD = 0x7, /* get value at index and push it onto the stack */
+    OP_STORE = 0x8 /* pop value off stack and load it into index */
 };
 
 Instruction create_instruction (Opcode op, int32_t imm);
