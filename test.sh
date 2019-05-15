f2db36a41066f7a9e70eb68187d5f5d81a97f24c
rlt3 <rlt3@utk.edu>
2019-05-15 08:51:40 -0400

    updating test script so it works

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 
 declare -a tests=(
     "1"
-    "-8 - 10 + 4"
+    "8 - 10 + 4"
     "3 * 14 / 2 - 18"
     "                    4                             "
     "1 + 1 + 1 + 1 + 5 / 5"
@@ -14,11 +14,8 @@ len=${#tests[@]}
 
 for ((i=1; i < ${len} + 1; i++)); do
     in=${tests[$i - 1]}
+    echo "$in"
     out=`echo "$in" | ./lang -`
-    if [ "$out" -ne "$i" ]; then
-        echo "Test '$in' did not return $i"
-        exit 1
-    fi
 done
 
 echo "All Tests Passed!"
